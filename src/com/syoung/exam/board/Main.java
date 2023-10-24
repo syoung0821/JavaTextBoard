63cab01a6e8e2b6257163494e89be45c4b2a5941
syoung <syoung0821@gmail.com>
2023-10-24 20:44:04 +0900

    명령어 추가2

--- src/com/syoung/exam/board/Main.java
+++ src/com/syoung/exam/board/Main.java
@@ -1,10 +1,19 @@
 package com.syoung.exam.board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
     System.out.println("== 게시판 v.01 ==");
     System.out.println("== 프로그램 시작 ==");
+    System.out.printf("명령 ) ");
+
+    String str = scan.nextLine();
+    System.out.println("입력된 명령어 : " + str);
     System.out.println("== 프로그램 종료 ==");
+    scan.close();
   }
 
 }
