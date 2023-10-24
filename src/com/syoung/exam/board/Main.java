6ecb4db60bdad4cfcb546388a4490ff3995a389c
syoung <syoung0821@gmail.com>
2023-10-24 20:30:54 +0900

    명령어 추가

--- src/com/syoung/exam/board/Main.java
+++ src/com/syoung/exam/board/Main.java
@@ -2,6 +2,9 @@ package com.syoung.exam.board;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("== 게시판 v.01 ==");
+    System.out.println("== 프로그램 시작 ==");
+    System.out.println("== 프로그램 종료 ==");
   }
+
 }
